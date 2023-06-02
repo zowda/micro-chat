afb52fd243f8e001784a1b93f0bc46cfc0c61fda
Zaher <zowda@users.noreply.github.com>
2023-06-02 09:01:39 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendString(":)")
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+radio.setGroup(1)
